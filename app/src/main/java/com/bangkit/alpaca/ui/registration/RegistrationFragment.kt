4457aaf08304347c84ff5328d9b5b53d4e8377e1
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-05-17 19:46:46 +0700

    implement navigation from registration to login

--- app/src/main/java/com/bangkit/alpaca/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/bangkit/alpaca/ui/registration/RegistrationFragment.kt
@@ -6,10 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bangkit.alpaca.R
 import com.bangkit.alpaca.databinding.FragmentRegistrationBinding
 
 
-class RegistrationFragment : Fragment() {
+class RegistrationFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentRegistrationBinding? = null
     private val binding get() = _binding
@@ -22,6 +23,26 @@ class RegistrationFragment : Fragment() {
         registrationViewModel = ViewModelProvider(this)[RegistrationViewModel::class.java]
         _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding?.btnToLoginFromRegistration?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_to_login_from_registration -> moveToLogin()
+        }
+    }
+
+    private fun moveToLogin() {
+        val mFragmentManager = parentFragmentManager
+        mFragmentManager.popBackStack()
     }
 
 }
