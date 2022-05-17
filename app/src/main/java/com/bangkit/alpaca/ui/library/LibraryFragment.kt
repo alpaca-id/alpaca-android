4974ad8237821cf6ac754b75e2a71bfcf8ed4007
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-05-17 10:56:10 +0700

    slicing UI for the fragment_collection.xml page

--- app/src/main/java/com/bangkit/alpaca/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/bangkit/alpaca/ui/library/LibraryFragment.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.bangkit.alpaca.databinding.FragmentLibraryBinding
 
 class LibraryFragment : Fragment() {
 
     private var _binding: FragmentLibraryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +17,8 @@ class LibraryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val libraryViewModel =
-            ViewModelProvider(this)[LibraryViewModel::class.java]
-
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        libraryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
