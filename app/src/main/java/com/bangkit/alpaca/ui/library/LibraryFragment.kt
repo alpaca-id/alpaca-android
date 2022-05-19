d313d15cc409f4164235db23f5d8bae75dcbcd32
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-05-19 15:02:19 +0700

    implement search library with dummy data

--- app/src/main/java/com/bangkit/alpaca/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/bangkit/alpaca/ui/library/LibraryFragment.kt
@@ -1,11 +1,15 @@
 package com.bangkit.alpaca.ui.library
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.alpaca.R
 import com.bangkit.alpaca.databinding.FragmentLibraryBinding
 import com.bangkit.alpaca.ui.adapter.StoriesAdapter
 import com.bangkit.alpaca.utils.DataDummy
@@ -30,6 +34,7 @@ class LibraryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupStories()
+        setupSearchBar()
     }
 
     private fun setupStories() {
@@ -37,6 +42,43 @@ class LibraryFragment : Fragment() {
         binding?.rvStoryLibrary?.apply {
             adapter = storiesAdapter
             layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun setupSearchBar() {
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView =
+            binding?.toolbarProfile?.menu?.findItem(R.id.library_search)?.actionView as SearchView
+
+        with(searchView) {
+            setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+            queryHint = "Cari cerita"
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if (query != null) {
+                        searchNotFound(query)
+                    }
+                    clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+
+            })
+        }
+    }
+
+    private fun searchNotFound(query: String) {
+        binding?.apply {
+            tvSearchResult.visibility = View.VISIBLE
+            containerSearchNotFound.visibility = View.VISIBLE
+            tvNewCollection.visibility = View.GONE
+            rvStoryLibrary.visibility = View.GONE
+
+            tvSearchResult.text = getString(R.string.search_result, 0)
+            tvSearchNotFound.text = getString(R.string.result_not_found2, query)
         }
     }
 
