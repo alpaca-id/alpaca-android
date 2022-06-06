7fd5d645f1e0a61b0a498df8968286b843e661d0
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-06-06 13:46:29 +0700

    implement custom splash screen

--- app/src/main/java/com/bangkit/alpaca/ui/auth/AuthenticationActivity.kt
+++ app/src/main/java/com/bangkit/alpaca/ui/auth/AuthenticationActivity.kt
@@ -2,12 +2,15 @@ package com.bangkit.alpaca.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bangkit.alpaca.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
     }
